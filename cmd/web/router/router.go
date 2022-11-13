b45256bb395dfc7f1960bbfc02b801ac6d1a6b5f
Kompiang Gede Sukadharma <kompiang.sukadharma@gmail.com>
2022-11-13 08:58:35 +0800

    [fix] grouping that endpoints path

--- cmd/web/router/router.go
+++ cmd/web/router/router.go
@@ -18,33 +18,40 @@ type RouteParams struct {
 }
 
 func InitRoute(params RouteParams) {
-	params.E.GET(path.Ping, handler.PingHandler())
+	pathGroups := path.InitPathGroups(params.E)
 
-	// Auth
-	params.E.POST(path.Register, handler.Register(params.Service))
-	params.E.POST(path.Login, handler.Login(params.Service))
-	params.E.POST(path.RefreshToken, handler.RefreshToken(params.Service))
-	// User Profile
-	params.E.PUT(path.DeactivateUser, handler.DeactivateUser(params.Service), middleware.MustAuthorized(&params.Config.JWTConfig))
-	params.E.GET(path.GetProfileByUsername, handler.GetProfile(params.Service), middleware.MustAuthorized(&params.Config.JWTConfig))
-	params.E.PUT(path.EditProfile, handler.EditProfile(params.Service), middleware.MustAuthorized(&params.Config.JWTConfig))
-	// Post
-	params.E.POST(path.CreatePost, handler.CreatePost(params.Service), middleware.MustAuthorized(&params.Config.JWTConfig))
-	params.E.DELETE(path.DeletePost, handler.DeletePost(params.Service), middleware.MustAuthorized(&params.Config.JWTConfig))
-	params.E.GET(path.GetPostByID, handler.GetPostById(params.Service), middleware.MustAuthorized(&params.Config.JWTConfig))
-	params.E.GET(path.GetPostByUsername, handler.GetPostByUsername(params.Service), middleware.MustAuthorized(&params.Config.JWTConfig))
-	// Comment
-	params.E.POST(path.CreateComment, handler.CreateComment(params.Service), middleware.MustAuthorized(&params.Config.JWTConfig))
-	params.E.GET(path.GetChildCommentByParentID, handler.GetChildCommentByCommentID(params.Service), middleware.MustAuthorized(&params.Config.JWTConfig))
-	// Timeline
-	params.E.GET(path.Timeline, handler.GetTimeline(params.Service), middleware.MustAuthorized(&params.Config.JWTConfig))
-	// Notifications
-	params.E.GET(path.GetNotifications, handler.GetNotifications(params.Service), middleware.MustAuthorized(&params.Config.JWTConfig))
-	params.E.POST(path.CreateGeneralNotifications, handler.AdminCreateGeneralNotifications(params.Service), middleware.AdminSecretTokenAuth(&params.Config.Admin), middleware.AdminBasicAuth(&params.Config.Admin))
-	// User Relation
-	params.E.GET(path.GetFollowers, handler.GetFollowers(params.Service), middleware.MustAuthorized(&params.Config.JWTConfig))
-	params.E.GET(path.GetFollowings, handler.GetFollowing(params.Service), middleware.MustAuthorized(&params.Config.JWTConfig))
-	params.E.POST(path.FollowOtherUser, handler.FollowOtherUser(params.Service), middleware.MustAuthorized(&params.Config.JWTConfig))
-
-	params.E.GET(path.WebSocket, handler.WebSocket(params.Pool), middleware.MustAuthorized(&params.Config.JWTConfig))
+	pathGroups.PingGroupV1.GET("", handler.PingHandler())
+	// Auth => /api/v1/auth
+	pathGroups.AuthGroupV1.POST("/register", handler.Register(params.Service))
+	pathGroups.AuthGroupV1.POST("/login", handler.Login(params.Service))
+	pathGroups.AuthGroupV1.POST("/refresh", handler.RefreshToken(params.Service))
+	// User Profile => /api/v1/user
+	pathGroups.UserProfileGroupV1.Use(middleware.MustAuthorized(&params.Config.JWTConfig))
+	pathGroups.UserProfileGroupV1.PUT("/deactivate", handler.DeactivateUser(params.Service))
+	pathGroups.UserProfileGroupV1.PUT("", handler.EditProfile(params.Service))
+	pathGroups.UserProfileGroupV1.GET("/:username", handler.GetProfile(params.Service))
+	// Post => /api/v1/post
+	pathGroups.PostGroupV1.Use(middleware.MustAuthorized(&params.Config.JWTConfig))
+	pathGroups.PostGroupV1.POST("", handler.CreatePost(params.Service))
+	pathGroups.PostGroupV1.DELETE("", handler.DeletePost(params.Service))
+	pathGroups.PostGroupV1.GET("/:id", handler.GetPostById(params.Service))
+	pathGroups.PostGroupV1.GET("/:username/post", handler.GetPostByUsername(params.Service))
+	// Comment => /api/v1/comment
+	pathGroups.CommentGroupV1.Use(middleware.MustAuthorized(&params.Config.JWTConfig))
+	pathGroups.CommentGroupV1.POST("", handler.CreateComment(params.Service))
+	pathGroups.CommentGroupV1.GET("/:parent_comment_id", handler.GetChildCommentByCommentID(params.Service))
+	// Timeline => /api/v1/timeline
+	pathGroups.TimelineGroupV1.Use(middleware.MustAuthorized(&params.Config.JWTConfig))
+	pathGroups.TimelineGroupV1.GET("", handler.GetTimeline(params.Service))
+	// Notifications => /api/v1/notifications
+	pathGroups.NotificationGroupV1.GET("/notifications", handler.GetNotifications(params.Service), middleware.MustAuthorized(&params.Config.JWTConfig))
+	pathGroups.NotificationGroupV1.POST("/notifications", handler.AdminCreateGeneralNotifications(params.Service), middleware.AdminSecretTokenAuth(&params.Config.Admin), middleware.AdminBasicAuth(&params.Config.Admin))
+	// User Relation => /api/v1
+	pathGroups.UserRelationGroupV1.Use(middleware.MustAuthorized(&params.Config.JWTConfig))
+	pathGroups.UserRelationGroupV1.GET("/followers/:username", handler.GetFollowers(params.Service))
+	pathGroups.UserRelationGroupV1.GET("/followings/:username", handler.GetFollowing(params.Service))
+	pathGroups.UserRelationGroupV1.POST("/follow", handler.FollowOtherUser(params.Service))
+	// Websocket => /api/v1/ws
+	pathGroups.WebSocketGroup.Use(middleware.MustAuthorized(&params.Config.JWTConfig))
+	pathGroups.WebSocketGroup.GET("", handler.WebSocket(params.Pool))
 }
